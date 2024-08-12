3580fbe28027a4a0b1ac2aaa244835141a772f05
Rodrigo Gomes da Silva <rodrigo.smscom@gmail.com>
2024-08-12 19:47:21 -0300

    asdasd

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,42 +1,42 @@
 import { defineBackend } from '@aws-amplify/backend'
 import { Stack } from 'aws-cdk-lib'
-import { EmailIdentity } from 'aws-cdk-lib/aws-ses'
+// import { EmailIdentity } from 'aws-cdk-lib/aws-ses'
 import { Effect, Policy, PolicyStatement } from 'aws-cdk-lib/aws-iam'
-import { auth } from './auth/resource'
+// import { auth } from './auth/resource'
 import { data } from './data/resource'
 import * as functions from './functions/resources'
 
 const backend = defineBackend({
-  auth,
+  // auth,
   data,
   ...functions,
 })
 
-const { cfnUserPool } = backend.auth.resources.cfnResources
-cfnUserPool.policies = {
-  passwordPolicy: {
-    minimumLength: 6,
-    requireLowercase: false,
-    requireNumbers: false,
-    requireSymbols: false,
-    requireUppercase: false,
-    temporaryPasswordValidityDays: 20,
-  },
-}
+// const { cfnUserPool } = backend.auth.resources.cfnResources
+// cfnUserPool.policies = {
+//   passwordPolicy: {
+//     minimumLength: 6,
+//     requireLowercase: false,
+//     requireNumbers: false,
+//     requireSymbols: false,
+//     requireUppercase: false,
+//     temporaryPasswordValidityDays: 20,
+//   },
+// }
 
-const authStack = Stack.of(cfnUserPool)
+// const authStack = Stack.of(cfnUserPool)
 
-const email = EmailIdentity.fromEmailIdentityName(
-  authStack,
-  'EmailIdentity',
-  // your email configured for use in SES
-  process.env.FROM_EMAIL || '',
-)
+// const email = EmailIdentity.fromEmailIdentityName(
+//   authStack,
+//   'EmailIdentity',
+//   // your email configured for use in SES
+//   process.env.FROM_EMAIL || '',
+// )
 
-cfnUserPool.emailConfiguration = {
-  emailSendingAccount: 'DEVELOPER',
-  sourceArn: email.emailIdentityArn,
-}
+// cfnUserPool.emailConfiguration = {
+//   emailSendingAccount: 'DEVELOPER',
+//   sourceArn: email.emailIdentityArn,
+// }
 
 backend.castVote.resources.lambda.role?.attachInlinePolicy(
   new Policy(
