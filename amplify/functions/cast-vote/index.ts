c831b102bde32b4b5f69b38f1ca3045c7960ef31
Rodrigo Gomes da Silva <rodrigo.smscom@gmail.com>
2024-08-11 23:17:35 -0300

    asdasda

--- amplify/functions/cast-vote/index.ts
+++ amplify/functions/cast-vote/index.ts
@@ -3,7 +3,4 @@ import { defineFunction } from '@aws-amplify/backend'
 export const castVote = defineFunction({
   name: 'cast-vote',
   entry: './handler.ts',
-  environment: {
-    VOTE_TABLE_NAME: process.env.VOTE_TABLE_NAME || 'Vote',
-  },
 })
--- amplify/functions/seed/index.ts
+++ amplify/functions/seed/index.ts
@@ -3,7 +3,4 @@ import { defineFunction } from '@aws-amplify/backend'
 export const seed = defineFunction({
   name: 'seed',
   entry: './handler.ts',
-  environment: {
-    CANDIDATE_TABLE_NAME: process.env.CANDIDATE_TABLE_NAME || 'Candidate',
-  },
 })
