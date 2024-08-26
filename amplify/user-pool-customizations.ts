b3a259fe1614472f4edd954a81dc017b2564148e
Rodrigo Gomes da Silva <rodrigo.smscom@gmail.com>
2024-08-26 01:08:16 -0300

    meh

--- amplify/user-pool-customizations.ts
+++ amplify/user-pool-customizations.ts
@@ -2,7 +2,7 @@ import type { backend as Backend } from './backend'
 import type { backend as BackendInit } from './backend-init'
 import { EmailIdentity } from 'aws-cdk-lib/aws-ses'
 import { Stack } from 'aws-cdk-lib'
-import { passwordPolicy } from '@/src/password-policy'
+import { passwordPolicy } from '../src/password-policy'
 
 export const applyUserPoolCustomizations = (backend: typeof Backend | typeof BackendInit) => {
   const { cfnUserPool } = backend.auth.resources.cfnResources
